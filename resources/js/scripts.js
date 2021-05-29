02e77356bc22e84a3080e9332352fd312bd93755
hrishikeshd36 <83512961+hrishikeshd36@users.noreply.github.com>
2021-05-30 00:46:36 +0530

    Update scripts.js

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -73,6 +73,32 @@ $(document).ready(function () {
             icon.removeClass('ion-close-round')
         }
     });
+    
+    $(window).resize(function(){
+
+        var nav = $('.js--main-nav');
+
+        var icon = $('.js--nav-icon i');
+
+        if ($(window).width() > 767){
+
+            nav.css("display", "block");
+
+            icon.addClass('ion-close-round');
+
+            icon.removeClass('ion-navicon-round');
+
+        } else {
+
+            nav.css("display", "none");
+
+            icon.addClass('ion-navicon-round');
+
+            icon.removeClass('ion-close-round');
+
+        }
+
+    });
 
 
 });
